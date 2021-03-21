94791c710d321c70b4466bf0c7aee04a4011208e
Stutikashyap17 <81111631+Stutikashyap17@users.noreply.github.com>
2021-03-21 12:23:49 -0700

    Add files via upload

---
+++ login_quiz.js
@@ -0,0 +1,14 @@
+function user_login () {
+    var quizzer1, quizzer2;
+
+    quizzer1 = document.getElementById("Username_1").value;
+    quizzer2 = document.getElementById("Username_2").value;
+
+
+
+     localStorage.setItem("quizzer1", quizzer1);
+     localStorage.setItem("quizzer2", quizzer2);
+    
+     window.location="MathQuiz-page.html";
+     
+    }
